9a243986176b2b43feb2479a8321c12b35e82700
Jatin Rajput <ec2-user@ip-172-31-8-202.ap-south-1.compute.internal>
2025-11-13 12:09:47 +0000

    Login logout fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { initializeAuth } from '@/_services/api';   
+initializeAuth(); 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
