6a713f98697846c3f9aea247a4ed76d642250739
build server <build.server@learnqoch.com>
2025-12-18 11:28:28 +0000

    fixed crash issue commented exam tab nab

--- src/Teacher/screens/Pages/ExamManageMent/ExamManagementLayout.jsx
+++ src/Teacher/screens/Pages/ExamManageMent/ExamManagementLayout.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import TabNav from "./Components/TabNav";
+// import TabNav from "./Components/TabNav";
 
 export default function ExamManagementLayout() {
   return (
     <div className="p-0 sm:p-6">
       <h2 className="pageheading mb-2 sm:mb-4">Exam Management </h2>
-      <TabNav/>
+      <TabNav />
 
       <div className="mt-1 sm:mt-3 sm:bg-white sm:p-4 sm:rounded-xl sm:shadow-sm sm:border sm:border-gray-100">
         <Outlet />
