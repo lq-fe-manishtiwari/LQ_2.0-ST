90c6128e9a691159f511b3b28223e2c5c8d7e7f7
Shilpa <shilpa.sonule@learnqoch.com>
2026-01-07 10:35:17 +0530

    api

--- src/Teacher/screens/Pages/ExamManageMent/Services/Exam.service.js
+++ src/Teacher/screens/Pages/ExamManageMent/Services/Exam.service.js
@@ -1,15 +1,15 @@
 import {
-    authHeader,
-    authHeaderToPost,
-    handleResponse,
-    ExamMGMAPI
-  } from "@/_services/api";
-  
+  authHeader,
+  authHeaderToPost,
+  handleResponse,
+  ExamMGMAPI
+} from "@/_services/api";
+
 export const examgService = {
   getTeacherDutyAllocationsByCollege,
+  getTeacherDutyAllocationsByTeacher,
+  getExamSchedulesByTeacher,
 };
-  
-  
 
 /**
  * GET: Get Teacher Duty Allocations by College ID
@@ -27,3 +27,34 @@ function getTeacherDutyAllocationsByCollege(collegeId) {
   ).then(handleResponse);
 }
 
+/**
+ * GET: Get Teacher Duty Allocations by Teacher ID
+ * /api/admin/teacher-duty-allocations/by-teacher/{teacherId}
+ */
+function getTeacherDutyAllocationsByTeacher(teacherId) {
+  const requestOptions = {
+    method: "GET",
+    headers: authHeader()
+  };
+
+  return fetch(
+    `${ExamMGMAPI}/admin/teacher-duty-allocations/by-teacher/${teacherId}`,
+    requestOptions
+  ).then(handleResponse);
+}
+
+/**
+ * GET: Get Exam Schedules for a Teacher
+ * /api/exam-schedules/teacher/{teacherId}
+ */
+function getExamSchedulesByTeacher(teacherId) {
+  const requestOptions = {
+    method: "GET",
+    headers: authHeader()
+  };
+
+  return fetch(
+    `${ExamMGMAPI}/exam-schedules/teacher/${teacherId}`,
+    requestOptions
+  ).then(handleResponse);
+}
